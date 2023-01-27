6f71d26e5877478ff26f545d9b8a80373225ab89
Olga <olya_potapova@mail.ru>
2023-01-27 11:23:44 +0300

    HomeWork9

---
+++ HomeWork9/main.py
@@ -0,0 +1,16 @@
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5807751052:AAF0wKkxtkG_DVkAxBe5Ae644jXJx5xPmvo").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler('sum', sum_command))
+
+app.run_polling()
